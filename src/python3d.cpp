9f8777f6a4ed4b80e200362758bb477862e23bae
edupont.mail <edupont.mail@9e7ad364-e296-11de-9043-17b7bd24f792>
2009-12-13 01:19:53 +0000

    Ajout de la premiere version du gameListener. Gestion de la touche echap pour quitter.

--- src/python3d.cpp
+++ src/python3d.cpp
@@ -25,10 +25,13 @@ void Python3D::start()
 	_Root->showConfigDialog();
 
 	_RenderWindow = _Root->initialise(true,"Python3D  -   By #undefined team");
-	_SceneManager = _Root->createSceneManager(Ogre::ST_GENERIC, "SceneManager");
+	_SceneManager = _Root->createSceneManager("BspSceneManager");
 
 	_Camera = _SceneManager->createCamera("Camera");
 	_Viewport = _RenderWindow->addViewport(_Camera);
+
+	initOIS();
+	createFrameListener();
 }
 
 void Python3D::exit()
@@ -36,3 +39,35 @@ void Python3D::exit()
 	if (_Root!=0)
 		delete _Root;
 }	
+
+void Python3D::setupResources()
+{
+
+}
+
+void Python3D::loadResources()
+{
+
+}
+
+void Python3D::createFrameListener()
+{
+	GameListener *listener = new GameListener(_SceneManager, _Keyboard, _Mouse);
+	_Root->addFrameListener(listener);
+}
+
+void Python3D::initOIS()
+{
+	size_t windowHnd = 0;
+    std::ostringstream windowHndStr;
+    OIS::ParamList pl;
+	Ogre::RenderWindow *win = _Root->getAutoCreatedWindow();
+
+    win->getCustomAttribute("WINDOW", &windowHnd);
+    windowHndStr << windowHnd;
+    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+    _InputManager = OIS::InputManager::createInputSystem(pl);
+
+    _Keyboard = static_cast<OIS::Keyboard*>(_InputManager->createInputObject(OIS::OISKeyboard, true));
+    _Mouse = static_cast<OIS::Mouse*>(_InputManager->createInputObject(OIS::OISMouse, true));
+}
