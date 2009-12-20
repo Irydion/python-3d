0228f89f283076027066e9ad53c05c6467278c68
edupont.mail <edupont.mail@9e7ad364-e296-11de-9043-17b7bd24f792>
2009-12-20 21:46:24 +0000

    Ajout de la FMOD pour une future utilisation.

---
+++ trunk/src/soundManager.h
@@ -0,0 +1,42 @@
+/*
+-------------------------------------------------------------------------------
+This file is part of Python3D.
+
+Copyright (c) 2009 Emilien Dupont
+Also see acknowledgements in COPYING.txt
+
+Python3D is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Python3D is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Python3D. If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------
+*/
+
+/**
+ * \file soundManager.h
+ * \brief SoundManager : le gestionnaire de son
+ * \author Emilien Dupont
+ */
+
+#ifndef SOUNDMANAGER_H
+#define SOUNDMANAGER_H
+
+#include <FMOD/fmod.hpp>
+
+class SoundManager
+{
+	public:
+		SoundManager();
+		~SoundManager();
+
+};
+
+#endif // SOUNDMANAGER_H
